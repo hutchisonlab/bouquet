df8d5bd8cd7093a8f4fc114f637bf244d6d2bed2
Geoff Hutchison <geoff.hutchison@gmail.com>
2024-01-11 11:58:24 -0500

    Revising environment

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bouquet',
-    version='0.1.0',
+    version='0.5.0',
     packages=find_packages(),
     description='Conformer optimization via Bayesian optimization',
 )
